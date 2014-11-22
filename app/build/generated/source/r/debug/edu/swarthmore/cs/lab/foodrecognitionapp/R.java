9e3888dbcc66a20dafe69d218812715f0330a896
Jonah Schwartz <jschwar1@cs.swarthmore.edu>
2014-11-22 14:51:14 -0500

    added openCV library to project

--- app/build/generated/source/r/debug/edu/swarthmore/cs/lab/foodrecognitionapp/R.java
+++ app/build/generated/source/r/debug/edu/swarthmore/cs/lab/foodrecognitionapp/R.java
@@ -9,35 +9,64 @@ package edu.swarthmore.cs.lab.foodrecognitionapp;
 
 public final class R {
     public static final class attr {
+        /** <p>May be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+<p>May be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>any</code></td><td>-1</td><td></td></tr>
+<tr><td><code>back</code></td><td>99</td><td></td></tr>
+<tr><td><code>front</code></td><td>98</td><td></td></tr>
+</table>
+         */
+        public static final int camera_id=0x7f010001;
+        /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int show_fps=0x7f010000;
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int add_food_photo=0x7f08000e;
-        public static final int add_tags_button=0x7f080006;
-        public static final int breakfast_menu=0x7f080008;
-        public static final int dinner_menu=0x7f08000a;
-        public static final int food_photo_list_dateTextView=0x7f08000b;
-        public static final int food_photo_list_tagsTextView=0x7f08000c;
-        public static final int fragmentContainer=0x7f080000;
-        public static final int imageView1=0x7f080003;
-        public static final int list_image=0x7f08000d;
-        public static final int lunch_menu=0x7f080009;
-        public static final int menu_item_add=0x7f080011;
-        public static final int menu_item_deleteAll=0x7f080013;
-        public static final int menu_item_gallery=0x7f080012;
-        public static final int pictureTag=0x7f080005;
-        public static final int retake_photo_button=0x7f080002;
-        public static final int root=0x7f080001;
-        public static final int saveButton=0x7f080007;
-        public static final int tagContainerLayout=0x7f080004;
-        public static final int todays_menu=0x7f080010;
-        public static final int view_nutrition_history=0x7f08000f;
+        public static final int add_food_photo=0x7f040011;
+        public static final int add_tags_button=0x7f040009;
+        public static final int any=0x7f040000;
+        public static final int back=0x7f040001;
+        public static final int breakfast_menu=0x7f04000b;
+        public static final int dinner_menu=0x7f04000d;
+        public static final int food_photo_list_dateTextView=0x7f04000e;
+        public static final int food_photo_list_tagsTextView=0x7f04000f;
+        public static final int fragmentContainer=0x7f040003;
+        public static final int front=0x7f040002;
+        public static final int imageView1=0x7f040006;
+        public static final int list_image=0x7f040010;
+        public static final int lunch_menu=0x7f04000c;
+        public static final int menu_item_add=0x7f040014;
+        public static final int menu_item_deleteAll=0x7f040016;
+        public static final int menu_item_gallery=0x7f040015;
+        public static final int pictureTag=0x7f040008;
+        public static final int retake_photo_button=0x7f040005;
+        public static final int root=0x7f040004;
+        public static final int saveButton=0x7f04000a;
+        public static final int tagContainerLayout=0x7f040007;
+        public static final int todays_menu=0x7f040013;
+        public static final int view_nutrition_history=0x7f040012;
     }
     public static final class layout {
         public static final int activity_foodphoto=0x7f030000;
@@ -49,28 +78,83 @@ public final class R {
         public static final int main_menu=0x7f030006;
     }
     public static final class menu {
-        public static final int picture_taker=0x7f070000;
+        public static final int picture_taker=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int add_food_photo=0x7f050001;
-        public static final int add_tags_button=0x7f050002;
-        public static final int app_name=0x7f050003;
-        public static final int food_photo_title=0x7f050004;
-        public static final int main_menu_title=0x7f050005;
-        public static final int menu_item_add=0x7f050006;
-        public static final int menu_item_deleteAll=0x7f050007;
-        public static final int menu_item_gallery=0x7f050008;
-        public static final int openCamera=0x7f050009;
-        public static final int pictureTagHint=0x7f05000a;
-        public static final int retake_photo_button=0x7f05000b;
-        public static final int saveButton=0x7f05000c;
-        public static final int todays_menu=0x7f05000d;
-        public static final int view_nutrition_history=0x7f05000e;
+        public static final int action_settings=0x7f060000;
+        public static final int add_food_photo=0x7f060001;
+        public static final int add_tags_button=0x7f060002;
+        public static final int app_name=0x7f060003;
+        public static final int food_photo_title=0x7f060004;
+        public static final int main_menu_title=0x7f060005;
+        public static final int menu_item_add=0x7f060006;
+        public static final int menu_item_deleteAll=0x7f060007;
+        public static final int menu_item_gallery=0x7f060008;
+        public static final int openCamera=0x7f060009;
+        public static final int pictureTagHint=0x7f06000a;
+        public static final int retake_photo_button=0x7f06000b;
+        public static final int saveButton=0x7f06000c;
+        public static final int todays_menu=0x7f06000d;
+        public static final int view_nutrition_history=0x7f06000e;
     }
     public static final class style {
         /**  Customize your theme here. 
          */
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a CameraBridgeViewBase.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #CameraBridgeViewBase_camera_id edu.swarthmore.cs.lab.foodrecognitionapp:camera_id}</code></td><td></td></tr>
+           <tr><td><code>{@link #CameraBridgeViewBase_show_fps edu.swarthmore.cs.lab.foodrecognitionapp:show_fps}</code></td><td></td></tr>
+           </table>
+           @see #CameraBridgeViewBase_camera_id
+           @see #CameraBridgeViewBase_show_fps
+         */
+        public static final int[] CameraBridgeViewBase = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>This symbol is the offset where the {@link edu.swarthmore.cs.lab.foodrecognitionapp.R.attr#camera_id}
+          attribute's value can be found in the {@link #CameraBridgeViewBase} array.
+
+
+          <p>May be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+<p>May be one of the following constant values.</p>
+<table>
+<colgroup align="left" />
+<colgroup align="left" />
+<colgroup align="left" />
+<tr><th>Constant</th><th>Value</th><th>Description</th></tr>
+<tr><td><code>any</code></td><td>-1</td><td></td></tr>
+<tr><td><code>back</code></td><td>99</td><td></td></tr>
+<tr><td><code>front</code></td><td>98</td><td></td></tr>
+</table>
+          @attr name edu.swarthmore.cs.lab.foodrecognitionapp:camera_id
+        */
+        public static final int CameraBridgeViewBase_camera_id = 1;
+        /**
+          <p>This symbol is the offset where the {@link edu.swarthmore.cs.lab.foodrecognitionapp.R.attr#show_fps}
+          attribute's value can be found in the {@link #CameraBridgeViewBase} array.
+
+
+          <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name edu.swarthmore.cs.lab.foodrecognitionapp:show_fps
+        */
+        public static final int CameraBridgeViewBase_show_fps = 0;
+    };
 }
