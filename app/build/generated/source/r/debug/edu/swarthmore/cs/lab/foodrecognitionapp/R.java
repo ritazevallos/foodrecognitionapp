8e8061a79443ce745fd47f4100bfa7ed1c7102b0
Jonah Schwartz <jschwar1@cs.swarthmore.edu>
2014-11-25 21:15:45 -0500

    Changed callback to the fancy face detection one

--- app/build/generated/source/r/debug/edu/swarthmore/cs/lab/foodrecognitionapp/R.java
+++ app/build/generated/source/r/debug/edu/swarthmore/cs/lab/foodrecognitionapp/R.java
@@ -37,36 +37,36 @@ containing a value of this type.
         public static final int show_fps=0x7f010000;
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int add_food_photo=0x7f040011;
-        public static final int any=0x7f040000;
-        public static final int back=0x7f040001;
-        public static final int breakfast_menu=0x7f04000b;
-        public static final int dinner_menu=0x7f04000d;
-        public static final int food_photo_list_dateTextView=0x7f04000e;
-        public static final int food_photo_list_tagsTextView=0x7f04000f;
-        public static final int fragmentContainer=0x7f040003;
-        public static final int front=0x7f040002;
-        public static final int imageView1=0x7f040006;
-        public static final int list_image=0x7f040010;
-        public static final int lunch_menu=0x7f04000c;
-        public static final int menu_item_add=0x7f040014;
-        public static final int menu_item_deleteAll=0x7f040016;
-        public static final int menu_item_gallery=0x7f040015;
-        public static final int pictureTag=0x7f040008;
-        public static final int retake_photo_button=0x7f040005;
-        public static final int root=0x7f040004;
-        public static final int saveButton=0x7f04000a;
-        public static final int segmentsContainerLayout=0x7f040009;
-        public static final int tagContainerLayout=0x7f040007;
-        public static final int todays_menu=0x7f040013;
-        public static final int view_nutrition_history=0x7f040012;
+        public static final int add_food_photo=0x7f050011;
+        public static final int any=0x7f050000;
+        public static final int back=0x7f050001;
+        public static final int breakfast_menu=0x7f05000b;
+        public static final int dinner_menu=0x7f05000d;
+        public static final int food_photo_list_dateTextView=0x7f05000e;
+        public static final int food_photo_list_tagsTextView=0x7f05000f;
+        public static final int fragmentContainer=0x7f050003;
+        public static final int front=0x7f050002;
+        public static final int imageView1=0x7f050006;
+        public static final int list_image=0x7f050010;
+        public static final int lunch_menu=0x7f05000c;
+        public static final int menu_item_add=0x7f050014;
+        public static final int menu_item_deleteAll=0x7f050016;
+        public static final int menu_item_gallery=0x7f050015;
+        public static final int pictureTag=0x7f050008;
+        public static final int retake_photo_button=0x7f050005;
+        public static final int root=0x7f050004;
+        public static final int saveButton=0x7f05000a;
+        public static final int segmentsContainerLayout=0x7f050009;
+        public static final int tagContainerLayout=0x7f050007;
+        public static final int todays_menu=0x7f050013;
+        public static final int view_nutrition_history=0x7f050012;
     }
     public static final class layout {
         public static final int activity_foodphoto=0x7f030000;
@@ -78,29 +78,32 @@ containing a value of this type.
         public static final int main_menu=0x7f030006;
     }
     public static final class menu {
-        public static final int picture_taker=0x7f080000;
+        public static final int picture_taker=0x7f090000;
+    }
+    public static final class raw {
+        public static final int lbpcascade_frontalface=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060000;
-        public static final int add_food_photo=0x7f060001;
-        public static final int add_tags_button=0x7f060002;
-        public static final int app_name=0x7f060003;
-        public static final int food_photo_title=0x7f060004;
-        public static final int main_menu_title=0x7f060005;
-        public static final int menu_item_add=0x7f060006;
-        public static final int menu_item_deleteAll=0x7f060007;
-        public static final int menu_item_gallery=0x7f060008;
-        public static final int openCamera=0x7f060009;
-        public static final int pictureTagHint=0x7f06000a;
-        public static final int retake_photo_button=0x7f06000b;
-        public static final int saveButton=0x7f06000c;
-        public static final int todays_menu=0x7f06000d;
-        public static final int view_nutrition_history=0x7f06000e;
+        public static final int action_settings=0x7f070000;
+        public static final int add_food_photo=0x7f070001;
+        public static final int add_tags_button=0x7f070002;
+        public static final int app_name=0x7f070003;
+        public static final int food_photo_title=0x7f070004;
+        public static final int main_menu_title=0x7f070005;
+        public static final int menu_item_add=0x7f070006;
+        public static final int menu_item_deleteAll=0x7f070007;
+        public static final int menu_item_gallery=0x7f070008;
+        public static final int openCamera=0x7f070009;
+        public static final int pictureTagHint=0x7f07000a;
+        public static final int retake_photo_button=0x7f07000b;
+        public static final int saveButton=0x7f07000c;
+        public static final int todays_menu=0x7f07000d;
+        public static final int view_nutrition_history=0x7f07000e;
     }
     public static final class style {
         /**  Customize your theme here. 
          */
-        public static final int AppTheme=0x7f070000;
+        public static final int AppTheme=0x7f080000;
     }
     public static final class styleable {
         /** Attributes that can be used with a CameraBridgeViewBase.
--- app/src/main/java/edu/swarthmore/cs/lab/foodrecognitionapp/PictureTakerActivity.java
+++ app/src/main/java/edu/swarthmore/cs/lab/foodrecognitionapp/PictureTakerActivity.java
@@ -3,6 +3,7 @@ package edu.swarthmore.cs.lab.foodrecognitionapp;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,9 +13,13 @@ import android.util.Log;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.objdetect.CascadeClassifier;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 /**
@@ -24,7 +29,35 @@ public class PictureTakerActivity extends Activity {
     private String TAG = "PictureTakerActivity";
 
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+//    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+//        @Override
+//        public void onManagerConnected(int status) {
+//            switch (status) {
+//                case LoaderCallbackInterface.SUCCESS:
+//                {
+//                    Log.i(TAG, "OpenCV loaded successfully");
+//
+//                    // Load native library after(!) OpenCV initialization
+//                    //System.loadLibrary("mixed_sample");
+//
+//                } break;
+//                default:
+//                {
+//                    super.onManagerConnected(status);
+//                } break;
+//            }
+//        }
+//    };
+//
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
+    }
+    private File                   mCascadeFile;
+    private CascadeClassifier      mJavaDetector;
+    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
@@ -33,7 +66,37 @@ public class PictureTakerActivity extends Activity {
                     Log.i(TAG, "OpenCV loaded successfully");
 
                     // Load native library after(!) OpenCV initialization
-                    //System.loadLibrary("mixed_sample");
+                    //System.loadLibrary("detection_based_tracker");
+
+                    try {
+                        // load cascade file from application resources
+                        InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                        mCascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+                        FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = is.read(buffer)) != -1) {
+                            os.write(buffer, 0, bytesRead);
+                        }
+                        is.close();
+                        os.close();
+
+                        mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+                        if (mJavaDetector.empty()) {
+                            Log.e(TAG, "Failed to load cascade classifier");
+                            mJavaDetector = null;
+                        } else
+                            Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+
+                        cascadeDir.delete();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+                    }
+
 
                 } break;
                 default:
@@ -43,13 +106,6 @@ public class PictureTakerActivity extends Activity {
             }
         }
     };
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
